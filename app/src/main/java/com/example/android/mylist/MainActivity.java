0632c90c359b4e380462be80ad88186010783f9b
Ranger44 <hollystraley@gmail.com>
2018-03-02 20:58:07 -0700

    Initial commit

---
+++ app/src/main/java/com/example/android/mylist/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.android.mylist;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by holly on 8/22/2017.
+ */
+
+public class MainActivity extends AppCompatActivity{
+
+
+}
